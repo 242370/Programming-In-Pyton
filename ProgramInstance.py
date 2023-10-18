3bb55af4f05816d2218245cbc9b67079e842946a
242370 Cybula Rafał <242370@edu.p.lodz.pl>
2023-10-18 18:49:09 +0200

    Quantitive dataframe begun

--- ProgramInstance.py
+++ ProgramInstance.py
@@ -2,6 +2,7 @@ from Zad1 import Zad1
 
 if __name__ == '__main__':
     snails = Zad1()
-    counter = snails.generate_dataframe()
-    print(counter)
-    i = 1
+    qualitive_dataframe = snails.generate_qualitive_dataframe()
+    print(qualitive_dataframe)
+    quantitive_dataframe = snails.generate_quantitive_dataframe()
+    print(quantitive_dataframe)
--- Zad1.py
+++ Zad1.py
@@ -1,11 +1,13 @@
 import pandas
 import numpy as np
+import Arrays
 
 
 class Zad1:
     def __init__(self):
-        self.data = pandas.read_csv("data.csv")
+        self.data = pandas.read_csv("data.csv", header=None)
         self.qualitativeData = self.data[self.data.columns[0]]
+        self.quantitiveData = self.data[self.data.columns[1:]]
 
     def genderof(self, index):
         return self.qualitativeData[index]
@@ -20,7 +22,24 @@ class Zad1:
     def gender_percentage(self, gender):
         return round(self.count_genders(gender) / len(self.qualitativeData) * 100, 2)
 
-    def generate_dataframe(self):
+    def generate_qualitive_dataframe(self):
         final_data = [[self.count_genders('M'), self.gender_percentage('M')], [self.count_genders('I'), self.gender_percentage('I')],
-                [self.count_genders('I'), self.gender_percentage('I')]]
+                      [self.count_genders('I'), self.gender_percentage('I')]]
         return pandas.DataFrame(final_data, index=['Male', 'Infant', 'Female'], columns=['count', '%'])
+
+    def isolate_property(self, column):
+        self.quantitiveData = pandas.DataFrame(self.quantitiveData)
+        return self.quantitiveData[self.quantitiveData.columns[column]]
+
+    #TODO: needs quartiles
+    def generate_quantitive_dataframe(self):
+        final_data = []
+        for i in range(self.quantitiveData.shape[1]):
+            property = self.isolate_property(i)
+            final_data.append([np.mean(property), np.std(property), np.min(property), np.median(property), np.max(property)])
+        return pandas.DataFrame(final_data,
+                                index=['Length', 'Diameter', 'Height', 'Whole weight', 'Shucked weight', 'Viscera weight', 'Shell weight', 'Rings'],
+                                columns=['mean', 'std', 'min', '50%', 'max'])
+
+
+

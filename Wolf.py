daec54268b1703d639bf7566f69198ae7de575c8
242370 Cybula Rafał <242370@edu.p.lodz.pl>
2023-12-02 13:17:09 +0100

    Added code division to methods

--- Wolf.py
+++ Wolf.py
@@ -3,11 +3,5 @@ class Wolf:
         self.position = (0.0, 0.0)
 
     def move(self, movement_value, direction):
-        if direction == 'Up':
-            self.position = (self.position[0], self.position[1] + movement_value)
-        elif direction == 'Right':
-            self.position = (self.position[0] + movement_value, self.position[1])
-        elif direction == 'Down':
-            self.position = (self.position[0], self.position[1] - movement_value)
-        else:
-            self.position = (self.position[0] - movement_value, self.position[1])
+        self.position = (self.position[0] + movement_value * direction[0],
+                         self.position[1] + movement_value * direction[1])
--- Zad2.py
+++ Zad2.py
@@ -9,7 +9,7 @@ class Zad2:
     def __init__(self):
         # Simulation Constants
         self.absolute_value = 10.0
-        self.number_of_rounds = 50
+        self.number_of_rounds = 90
         self.number_of_sheep = 15
         self.sheep_movement_value = 0.5
         self.wolf_movement_value = 1.0
@@ -19,31 +19,38 @@ class Zad2:
         for sheep in range(self.number_of_sheep):
             self.sheep.append(Sheep(self.absolute_value))
 
+    def first_stage(self):
+        for sheep in range(self.number_of_sheep):
+            self.sheep[sheep].move(self.sheep_movement_value)
+
+        shortest_distance = math.dist(self.wolf.position, self.sheep[0].position)
+        closest_sheep_index = 0
+        for sheep in range(1, self.number_of_sheep):
+            current_distance = math.dist(self.wolf.position, self.sheep[sheep].position)
+            if current_distance < shortest_distance:
+                shortest_distance = current_distance
+                closest_sheep_index = sheep
+
+        return shortest_distance, closest_sheep_index
+
+    def second_stage(self, shortest_distance, closest_sheep_index):
+        if shortest_distance <= self.wolf_movement_value:
+            self.wolf.position = self.sheep[closest_sheep_index].position
+            self.sheep.pop(closest_sheep_index)
+            self.number_of_sheep -= 1
+
+        else:
+            direction_vector = ((self.sheep[closest_sheep_index].position[0] - self.wolf.position[0]) / shortest_distance,
+                                (self.sheep[closest_sheep_index].position[1] - self.wolf.position[1]) / shortest_distance)
+
+            self.wolf.move(self.wolf_movement_value, direction_vector)
+
     def start_simulation(self):
         for round in range(self.number_of_rounds):
-            # First Stage
-            for sheep in range(self.number_of_sheep):
-                self.sheep[sheep].move(self.sheep_movement_value)
+            if self.number_of_sheep == 0:
+                print('End')
+                return
 
-            shortest_distance = math.dist(self.wolf.position, self.sheep[0].position)
-            closest_sheep_index = 0
-            for sheep in range(1, self.number_of_sheep):
-                current_distance = math.dist(self.wolf.position, self.sheep[sheep].position)
-                if current_distance < shortest_distance:
-                    shortest_distance = current_distance
-                    closest_sheep_index = sheep
+            shortest_distance, closest_sheep_index = self.first_stage()
 
-            # Second Stage
-            if shortest_distance <= self.wolf_movement_value:
-                self.wolf.position = self.sheep[closest_sheep_index].position
-                self.sheep.pop(closest_sheep_index)
-                self.number_of_sheep -= 1
-                if self.number_of_sheep == 0:
-                    print('End')
-                    return
-            else:
-                direction_vector = ((self.sheep[closest_sheep_index].position[0] - self.wolf.position[0]) / shortest_distance,
-                                    (self.sheep[closest_sheep_index].position[1] - self.wolf.position[1]) / shortest_distance)
-
-                self.wolf.position = (self.wolf.position[0] + self.wolf_movement_value * direction_vector[0],
-                                      self.wolf.position[1] + self.wolf_movement_value * direction_vector[1])
+            self.second_stage(shortest_distance, closest_sheep_index)

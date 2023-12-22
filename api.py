ff8be6958b6cabe01378c89ff80147acfdaa2dab
242370 Cybula Rafał <242370@edu.p.lodz.pl>
2023-12-22 12:29:15 +0100

    Added sample HTML

--- api.py
+++ api.py
@@ -5,7 +5,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'hello world'
+    return '''
+        <html>
+            <head>
+                <title>Programming in Python</title>
+            </head>
+            <body>
+                <h1>Hello World!</h1>
+            </body>
+        </html>
+    '''
 
 
 @app.route('/test')

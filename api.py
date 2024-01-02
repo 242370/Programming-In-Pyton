6f9aec8855bc40e5e2f3d67c856d8a48e3df4582
242370 Cybula Rafał <242370@edu.p.lodz.pl>
2024-01-02 14:56:13 +0100

    Added validation for new records

--- api.py
+++ api.py
@@ -31,15 +31,27 @@ def display_adding_form():
 
 @app.route('/add', methods=['POST'])
 def add_record():
-    categorical = request.form.get('param1', type=int)
-    continuous1 = request.form.get('param2', type=float)
-    continuous2 = request.form.get('param3', type=float)
+    categorical = request.form.get('categorical', type=int)
+    continuous1 = request.form.get('continuous1', type=float)
+    continuous2 = request.form.get('continuous2', type=float)
+
+    if not isinstance(categorical, int) or categorical < 1:
+        return redirect('/error')
+    if not isinstance(continuous1, float):
+        return redirect('/error')
+    if not isinstance(continuous2, float):
+        return redirect('/error')
+
     new_object = Object(categorical=categorical, continuous1=continuous1, continuous2=continuous2)
     with app.app_context():
         db.session.add(new_object)
         db.session.commit()
         return redirect('/')
 
+
+@app.route('/error', methods=['GET'])
+def display_error_page():
+    return render_template('error.html')
 
 # testing endpoint
 @app.route('/hello_world')
